8eef4ae6f9d24ef0dc9f830fb7b98fb2e1102573
fengmoge <youlangjx3@163.com>
2018-01-13 23:10:59 +0800

    parent

--- src/main/java/yipianyun/common/constant/PubConst.java
+++
@@ -1,7 +0,0 @@
-package yipianyun.common.constant;
-
-import java.text.SimpleDateFormat;
-
-public class PubConst {
-	public static final SimpleDateFormat serialSDF=new SimpleDateFormat("yyyyMMddHHmmssSSS"); 
-}
